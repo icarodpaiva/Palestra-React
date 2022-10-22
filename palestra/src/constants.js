b88b8b91c84bd8d8af2d1eeb8292c26b5f595257
icaro paiva <icaro.paiva@acct.global>
2022-10-21 22:04:13 -0300

    delete files

--- palestra/src/constants.js
+++
@@ -1,7 +0,0 @@
-export const items = [
-  { href: "/", text: "item 1" },
-  { href: "/", text: "item 2" },
-  { href: "/", text: "item 3" },
-  { href: "/", text: "item 4" },
-  { href: "/", text: "item 5" }
-]
